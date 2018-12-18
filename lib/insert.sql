5ca8607be3f1ff524c804428e07fa6a474f47c95
rbrown3 <rosannabrown@outlook.com>
2018-12-17 19:15:40 -0500

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name,age,gender,color,temperament,alive)VALUES ('Mr. Chocolate',14,'F','brown','happy',false);
+INSERT INTO bears (name,age,gender,color,temperament,alive)VALUES ('Rowdy',14,'M','blue','agitated',true);
+INSERT INTO bears(name,age,gender,color,temperament,alive)VALUES ('Tabitha',10,'F','white','happy',true);
+INSERT INTO bears(name,age,gender,color,temperament,alive)VALUES ('Sergeant Brown',94,'F','brown','gloomy',false);
+INSERT INTO bears(name,age,gender,color,temperament,alive)VALUES ('Melissa',23,'F','orange','depressed',true);
+INSERT INTO bears(name,age,gender,color,temperament,alive)VALUES ('Grinch',36,'F','green','lonely',true);
+INSERT INTO bears(name,age,gender,color,temperament,alive)VALUES ('Wendy',38,'F','pink','goofy',false);
+INSERT INTO bears(name,age,gender,color,temperament,alive)VALUES (NULL, 19 ,'F','brown','happy',false);
